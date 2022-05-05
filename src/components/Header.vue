5cbd74033005d6441059814160d59478ce3ca408
skye7012 <skye7012@gmail.com>
2022-05-06 02:05:24 +0300

    ter

---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="nav">
+		<a class="nav-item">
+          <span class="title is-2 has-text-info">Shoes</span>
+        </a>
+        <div class="nav-item search field has-addons">
+          <div class="control has-icons-left">
+            <input class="input" type="text" placeholder="Найти" style="width: 40vw;">
+            <span class="icon is-left">
+              <i class="fas fa-search" aria-hidden="true"></i>
+            </span>
+          </div>
+          <div class="control">
+            <button class="button is-info">
+              Найти
+            </button>
+          </div>
+        </div>
+		<div class="nav-item-end">
+			<a class="button is-inverted" style="margin-right: 10px">
+			<span class="icon">
+				<i class="icons fa-solid fa-user"></i>
+			</span>
+			<span>Войти</span>
+			</a>
+			<a class="button is-inverted">
+			<span class="icon">
+				<i class="fa-solid fa-cart-shopping"></i>
+			</span>
+			<span>Корзина</span>
+			</a>
+      	</div>
+	</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+
+.nav {
+	box-sizing: border-box;
+	width: 100%;
+	padding: 0 5vw;
+	display: inline-flex;
+	justify-content: space-between;
+	&-item {
+		margin: auto 10px;
+
+		&-end {
+			margin: auto 0;
+			margin-left: auto;
+		}
+	}
+}
+
+.search {
+	
+}
+
+.title {
+	word-break: normal;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
