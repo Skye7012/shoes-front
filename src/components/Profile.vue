1dd4f9cc2756f3c8d37a826fefdd13586be1ff63
skye7012 <skye7012@gmail.com>
2022-05-16 09:01:38 +0300

    newProfileLayout draft beg

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,30 +1,42 @@
 <template>
 	<div class="registration">
-		<input
-			v-model="login"
-			class="input"
-			placeholder="Логин"
-			type="text"
-			disabled
-			>
-		<input
-			v-model="name"
-			class="input"
-			v-bind:class="{ req: !name, changed: name != getUser.name }"
-			placeholder="Имя"
-			type="text">
-		<input
-			v-model="fname"
-			class="input"
-			v-bind:class="{ changed: fname != getUser.fname }"
-			placeholder="Фамилия"
-			type="text">
-		<input
-			v-model="phone"
-			class="input"
-			v-bind:class="{ changed: phone != getUser.phone }"
-			placeholder="Телефон"
-			type="text">
+		<div>
+			<label>Логин</label>
+			<input
+				v-model="login"
+				class="input"
+				placeholder="Логин"
+				type="text"
+				disabled
+				>
+		</div>
+		<div>
+			<label>Имя</label>
+			<input
+				v-model="name"
+				class="input"
+				v-bind:class="{ req: !name, changed: name != getUser?.name }"
+				placeholder="Имя"
+				type="text">
+		</div>
+		<div>
+			<label>Фамилия</label>
+			<input
+				v-model="fname"
+				class="input"
+				v-bind:class="{ changed: fname != getUser?.fname }"
+				placeholder="Фамилия"
+				type="text">
+		</div>
+		<div>
+			<label>Телефон</label>
+			<input
+				v-model="phone"
+				class="input"
+				v-bind:class="{ changed: phone != getUser?.phone }"
+				placeholder="Телефон"
+				type="text">
+		</div>
 		<button 
 			class="button">
 			Сохранить изменения
@@ -35,7 +47,7 @@
 		</button>
 		<button 
 			@click="doLogout()"
-			class="button">
+			class="button exit">
 			Выйти
 		</button>
 	</div>
@@ -87,12 +99,22 @@ import { mapActions, mapGetters, mapState } from 'vuex'
 @import '@/assets/my.scss';
 
 .registration {
-	margin: auto auto;
+	//margin: auto auto;
 	
-	display: flex;
-	flex-direction: column;
+	// display: flex;
+	// flex-direction: column;
+	// justify-content: center;
+	display: grid;
+	grid-template-columns: 200px 200px;
+	//grid-template-rows: auto;
+	column-gap: 10px;
+	grid-auto-rows: min-content;
 	justify-content: center;
+	align-content: center;
+}
 
+label {
+	text-align: center;
 }
 
 .input {
@@ -104,6 +126,8 @@ import { mapActions, mapGetters, mapState } from 'vuex'
 	text-align: center;
 
 	margin-bottom: 10px;
+	// padding-left: 10px;
+	// padding-right: 10px;
 }
 
 .button {
@@ -121,6 +145,12 @@ import { mapActions, mapGetters, mapState } from 'vuex'
 
 .req {
 	border-color: $danger;
+}
+
+.exit {
+	justify-self: center;
+	grid-column: 1/3;
+	width: 50%;
 }
 
 </style>
