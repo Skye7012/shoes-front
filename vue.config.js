ccc71ec538cda3857dcbbbb6d557d5d36d18f1d5
skye7012 <skye7012@gmail.com>
2022-05-21 23:53:59 +0300

    cors

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
-module.exports = {
-  devServer: {
-    proxy: 'http://shoes-backend-php/',
-  }
-}
-
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   configureWebpack: {
-//     devServer: {
-//       headers: { "Access-Control-Allow-Origin": "*" }
-//     }
+// module.exports = {
+//   devServer: {
+//     proxy: 'http://shoes-backend-php/',
 //   }
-//   // css: {
-//   //   loaderOptions: {
-//   //     sass: {
-//   //       additionalData: `@import '@/css/bulma/sass/elements/box.sass'`
-//   //     }
-//   //   }
-//   // }
-// })
+// }
+
+// // const { defineConfig } = require('@vue/cli-service')
+// // module.exports = defineConfig({
+// //   transpileDependencies: true,
+// //   configureWebpack: {
+// //     devServer: {
+// //       headers: { "Access-Control-Allow-Origin": "*" }
+// //     }
+// //   }
+// //   // css: {
+// //   //   loaderOptions: {
+// //   //     sass: {
+// //   //       additionalData: `@import '@/css/bulma/sass/elements/box.sass'`
+// //   //     }
+// //   //   }
+// //   // }
+// // })
